e762c98ad55cf82bbf3f31f6835c0c2da99dbe88
Michel Perez <michel.perez@gmx.ch>
2023-07-23 16:40:23 +0200

    fix path

--- onpolicy/scripts/train_mpe_scripts/experiments/train_mpe_ss_local_ip.sh
+++ onpolicy/scripts/train_mpe_scripts/experiments/train_mpe_ss_local_ip.sh
@@ -31,7 +31,7 @@ echo "env is ${env}, scenario is ${scenario}, max seed is ${seed_max}"
 echo "Current experiment: CRIPPO, IP, local obs only"
     
 echo "seed is ${seed}:"
-CUDA_VISIBLE_DEVICES=0 python ../train/train_mpe.py --env_name ${env} --user_name ${user_name} --use_wandb 0 \
+CUDA_VISIBLE_DEVICES=0 python ../../train/train_mpe.py --env_name ${env} --user_name ${user_name} --use_wandb 0 \
     --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
     --n_training_threads ${n_training_threads} --n_rollout_threads ${n_rollout_threads} \
     --num_mini_batch ${num_mini_batch} --episode_length ${episode_length} --num_env_steps ${num_env_steps} \
