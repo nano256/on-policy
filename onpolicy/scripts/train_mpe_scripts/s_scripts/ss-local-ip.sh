795e52104a977be9f6c576b49fc96cb3c4ad7dce
Michel Perez <miperez@eu-login-47.euler.ethz.ch>
2023-07-23 17:13:52 +0200

    add sbatch scripts

---
+++ onpolicy/scripts/train_mpe_scripts/s_scripts/ss-local-ip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:2048m
+#SBATCH --time=6:00:00
+#SBATCH --job-name="onpolicy_core_test"
+#SBATCH --mem-per-cpu=3072
+#SBATCH --mail-type=END
+#SBATCH --mail-user=miperez@ethz.ch
+
+source ../experiments/train_mpe_ss_local_ip.sh $1 $2 $3
