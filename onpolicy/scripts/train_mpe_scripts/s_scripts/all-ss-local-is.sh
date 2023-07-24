123bb07311ae67734b1d20fd5265a779f4b4935a
Michel Perez <michel.perez@gmx.ch>
2023-07-24 13:36:19 +0200

    refactor sbatch scripts

--- onpolicy/scripts/train_mpe_scripts/s_scripts/all-ss-local-is.sh
+++ onpolicy/scripts/train_mpe_scripts/s_scripts/all-ss-local-is.sh
@@ -2,12 +2,14 @@
 script_name="train_mpe_ss_local_is.sh"
 seed=0
 
-sbatch --wrap="./ss-ip.sh ${script_name} ${seed} 12 0.0"
-sbatch --wrap="./ss-ip.sh ${script_name} ${seed} 12 0.1"
-sbatch --wrap="./ss-ip.sh ${script_name} ${seed} 12 0.01"
-sbatch --wrap="./ss-ip.sh ${script_name} ${seed} 26 0.0"
-sbatch --wrap="./ss-ip.sh ${script_name} ${seed} 26 0.1"
-sbatch --wrap="./ss-ip.sh ${script_name} ${seed} 26 0.01"
+
+sbatch -n 1 --cpus-per-task=2 --gpus=1 --gres=gpumem:2048m --time=18:00:00 --mem-per-cpu=12288 --wrap="./call_experiment.sh ${script_name} ${seed} 12 0.0"
+sbatch -n 1 --cpus-per-task=2 --gpus=1 --gres=gpumem:2048m --time=18:00:00 --mem-per-cpu=12288 --wrap="./call_experiment.sh ${script_name} ${seed} 12 0.1"
+sbatch -n 1 --cpus-per-task=2 --gpus=1 --gres=gpumem:2048m --time=18:00:00 --mem-per-cpu=12288 --wrap="./call_experiment.sh ${script_name} ${seed} 12 0.01"
+
+sbatch -n 1 --cpus-per-task=2 --gpus=1 --gres=gpumem:2048m --time=18:00:00 --mem-per-cpu=12288 --wrap="./call_experiment.sh ${script_name} ${seed} 26 0.0"
+sbatch -n 1 --cpus-per-task=2 --gpus=1 --gres=gpumem:2048m --time=18:00:00 --mem-per-cpu=12288 --wrap="./call_experiment.sh ${script_name} ${seed} 26 0.1"
+sbatch -n 1 --cpus-per-task=2 --gpus=1 --gres=gpumem:2048m --time=18:00:00 --mem-per-cpu=12288 --wrap="./call_experiment.sh ${script_name} ${seed} 26 0.01"
 
 
 
