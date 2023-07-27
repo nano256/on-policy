0d3ceed7cc3252e102670b763ef6361393dd9720
Michel Perez <michel.perez@gmx.ch>
2023-07-27 22:34:45 +0200

    change name

--- onpolicy/scripts/train_mpe_scripts/s_scripts/sbatch-ss-local-is-ip.sh
+++ onpolicy/scripts/train_mpe_scripts/s_scripts/sbatch-ss-local-is-ip.sh
@@ -47,8 +47,8 @@ sbatch -n 1 --cpus-per-task=8 --time=48:00:00 --job-name="IS CPU" --mem-per-cpu=
 sbatch -n 1 --cpus-per-task=8 --time=48:00:00 --job-name="IS CPU" --mem-per-cpu=4096 --mail-type=BEGIN,END --mail-user=miperez@ethz.ch --wrap="./call_experiment.sh ${is_script_name} 4 12 0.0" 
 
 # IS, CI=26, CC=0.0
-# sbatch -n 1 --cpus-per-task=8 --time=48:00:00 --job-name="IS CPU" --mem-per-cpu=4096 --mail-type=BEGIN,END --mail-user=miperez@ethz.ch --wrap="./call_experiment.sh ${is_script_name} 0 26 0.0" 
-# sbatch -n 1 --cpus-per-task=8 --time=48:00:00 --job-name="IS CPU" --mem-per-cpu=4096 --mail-type=BEGIN,END --mail-user=miperez@ethz.ch --wrap="./call_experiment.sh ${is_script_name} 1 26 0.0" 
+sbatch -n 1 --cpus-per-task=8 --time=48:00:00 --job-name="IS CPU" --mem-per-cpu=4096 --mail-type=BEGIN,END --mail-user=miperez@ethz.ch --wrap="./call_experiment.sh ${is_script_name} 0 26 0.0" 
+sbatch -n 1 --cpus-per-task=8 --time=48:00:00 --job-name="IS CPU" --mem-per-cpu=4096 --mail-type=BEGIN,END --mail-user=miperez@ethz.ch --wrap="./call_experiment.sh ${is_script_name} 1 26 0.0" 
 sbatch -n 1 --cpus-per-task=8 --time=48:00:00 --job-name="IS CPU" --mem-per-cpu=4096 --mail-type=BEGIN,END --mail-user=miperez@ethz.ch --wrap="./call_experiment.sh ${is_script_name} 2 26 0.0" 
 sbatch -n 1 --cpus-per-task=8 --time=48:00:00 --job-name="IS CPU" --mem-per-cpu=4096 --mail-type=BEGIN,END --mail-user=miperez@ethz.ch --wrap="./call_experiment.sh ${is_script_name} 3 26 0.0" 
 sbatch -n 1 --cpus-per-task=8 --time=48:00:00 --job-name="IS CPU" --mem-per-cpu=4096 --mail-type=BEGIN,END --mail-user=miperez@ethz.ch --wrap="./call_experiment.sh ${is_script_name} 4 26 0.0" 
