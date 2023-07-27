9f5964a86c312b777f07137f01572262ca5d9466
Michel Perez <michel.perez@gmx.ch>
2023-07-27 21:59:45 +0200

    change batch script

--- onpolicy/scripts/train_mpe_scripts/s_scripts/sbatch-ss-local-is-test.sh
+++ onpolicy/scripts/train_mpe_scripts/s_scripts/sbatch-ss-local-is-test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-is_script_name="train_sisl_ss_local_is.sh"
-ip_script_name="train_sisl_ss_local_ip.sh"
+is_script_name="train_mpe_ss_local_is.sh"
+ip_script_name="train_mpe_ss_local_ip.sh"
 
-sbatch --ntasks=1 --exclusive --cpus-per-task=8 --gpus=1 --gres=gpumem:2000m --time=48:00:00 --mem-per-cpu=4096 \
-    --wrap="./call_experiment.sh ${is_script_name} 0 26 0.0"
-sbatch --ntasks=1 --exclusive --cpus-per-task=8 --time=48:00:00 --mem-per-cpu=4096 \
-    --wrap="./call_experiment.sh ${is_script_name} 1 26 0.0" 
+sbatch --cpus-per-task=8 --gpus=1 --gres=gpumem:2000m --time=48:00:00 --mem-per-cpu=4096 --wrap="./call_experiment.sh ${is_script_name} 0 26 0.0"
+sbatch --cpus-per-task=8 --time=48:00:00 --mem-per-cpu=4096 --wrap="./call_experiment.sh ${is_script_name} 1 26 0.0" 
             
