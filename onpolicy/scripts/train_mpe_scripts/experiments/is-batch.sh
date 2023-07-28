1b113a5db9e84ee8cd6fcdcc739557b2c8cbb6b9
Michel Perez <michel.perez@gmx.ch>
2023-07-28 12:31:52 +0200

    add batch script for local computation

---
+++ onpolicy/scripts/train_mpe_scripts/experiments/is-batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# source ./train_mpe_ss_local_is.sh 0 1 0.0
+# source ./train_mpe_ss_local_is.sh 1 1 0.0
+# # source ./train_mpe_ss_local_is.sh 2 1 0.0
+# source ./train_mpe_ss_local_is.sh 3 1 0.0
+# source ./train_mpe_ss_local_is.sh 4 1 0.0
+
+# source ./train_mpe_ss_local_is.sh 0 12 0.0
+source ./train_mpe_ss_local_is.sh 1 12 0.0
+source ./train_mpe_ss_local_is.sh 2 12 0.0
+source ./train_mpe_ss_local_is.sh 3 12 0.0
+source ./train_mpe_ss_local_is.sh 4 12 0.0
