2e67ca5b742b2f667d708e8b0c7e4b208ee54c31
Michel Perez <michel.perez@gmx.ch>
2023-08-01 17:12:17 +0200

    fix arg passing

--- onpolicy/scripts/train_mpe_scripts/s_scripts/call_experiment.sh
+++ onpolicy/scripts/train_mpe_scripts/s_scripts/call_experiment.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 script_name=$1
-seed=$2
-communication_interval=$3
-commitment_coef=$4
+shift
 
 module load gcc/8.2.0
 module load python_gpu/3.11.2
@@ -11,4 +9,4 @@ module load python_gpu/3.11.2
 source /cluster/home/miperez/venvs/onpolicy311/bin/activate
 
 
-source "../experiments/${script_name}" ${seed} ${communication_interval} ${commitment_coef}
+source "../experiments/${script_name}" "$@" 
